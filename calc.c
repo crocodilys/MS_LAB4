823d2d7ec9ae8e740774e62c6ca1a79b3e942a62
Dmitry Croco <dmitry.croco@engineit.by>
2014-06-04 12:14:19 +0300

    lab 4 finish commit

--- calc.c
+++ calc.c
@@ -23,6 +23,31 @@ static char arg1_input[WRITE_SIZE];
 static char arg2_input[WRITE_SIZE];
 static char operation_input[WRITE_SIZE];
 
+long calculate(void) 
+{
+	long a1 = 0;
+	long a2 = 0;
+	long res = 0;
+
+	if (arg1_input[strlen(arg1_input) - 2] == '\n') {
+		arg1_input[strlen(arg1_input) - 2] = (char)0;
+	}
+
+	kstrtol(arg1_input, 10, &a1);
+	kstrtol(arg2_input, 10, &a2);
+
+	if (operation_input[0] == '+') {
+		res = a1 + a2;
+	} else if (operation_input[0] == '-') {
+		res = a1 - a2;
+	} else if (operation_input[0] == '.') {
+		res = a1 * a2;
+	} else if (operation_input[0] == '/') {
+		res = a1 / a2;
+	}
+	return res;
+}
+
 /*
  * arg1 write handler
  */
@@ -68,29 +93,10 @@ int write_operation(struct file *file, const char *buf, unsigned long count, voi
 /*
  * result read handler
  */
-int read_result(char *buffer, char **buffer_location,
-                  off_t offset, int buffer_length, int *eof, void *data)
+ int read_result(char *buffer, char **buffer_location,
+				  off_t offset, int buffer_length, int *eof, void *data)
 {
-	long a1 = 0;
-	long a2 = 0;
-	long res = 0;
-
-	if (arg1_input[strlen(arg1_input) - 2] == '\n') {
-		arg1_input[strlen(arg1_input) - 2] = (char)0;
-	}
-
-	kstrtol(arg1_input, 10, &a1);
-	kstrtol(arg2_input, 10, &a2);
-
-	if (operation_input[0] == '+') {
-		res = a1 + a2;
-	} else if (operation_input[0] == '-') {
-		res = a1 - a2;
-	} else if (operation_input[0] == '.') {
-		res = a1 * a2;
-	} else if (operation_input[0] == '/') {
-		res = a1 / a2;
-	}
+	long res = calculate();
 
 	return sprintf(buffer, "%ld\n", res);
 }
